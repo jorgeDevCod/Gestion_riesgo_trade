532a376dacb8f61312ce7373cba9a366a1f1ca9b
jorgeGMesta <jpasapera@csticorp.biz>
2025-08-09 19:25:02 -0500

    Upload function cokies

--- firebase-app.js
+++ firebase-app.js
@@ -26,6 +26,7 @@ let currentUser = null;
 let isInitializing = true;
 let hasShownWelcomeBanner = false;
 let hasShownAuthModal = false;
+let authModalShownInSession = false;
 
 // Configuraciones de estrategias
 const strategyConfigs = {
@@ -124,6 +125,28 @@ function showAuthModal() {
         document.getElementById( 'authModal' ).classList.remove( 'hidden' );
         hasShownAuthModal = true;
     }
+}
+
+// Funciones para manejar cookies
+function setCookie( name, value, days = 365 ) {
+    const expires = new Date();
+    expires.setTime( expires.getTime() + ( days * 24 * 60 * 60 * 1000 ) );
+    document.cookie = `${name}=${value};expires=${expires.toUTCString()};path=/`;
+}
+
+function getCookie( name ) {
+    const nameEQ = name + "=";
+    const ca = document.cookie.split( ';' );
+    for ( let i = 0; i < ca.length; i++ ) {
+        let c = ca[ i ];
+        while ( c.charAt( 0 ) === ' ' ) c = c.substring( 1, c.length );
+        if ( c.indexOf( nameEQ ) === 0 ) return c.substring( nameEQ.length, c.length );
+    }
+    return null;
+}
+
+function deleteCookie( name ) {
+    document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
 }
 
 function hideAuthModal() {
