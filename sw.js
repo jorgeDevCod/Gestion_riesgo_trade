998c39b6408b1714dc354bf2164e1ffd3cc88be6
jorgeGMesta <jpasapera@csticorp.biz>
2025-10-23 18:23:31 -0500

    Update reglas disciplina

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'Gestor-tradeApp-V1.5'; // Cambia versión para forzar actualización
+const CACHE_NAME = 'Gestor-tradeApp-V1.6'; // Cambia versión para forzar actualización
 const urlsToCache = [
     '/',
     './index.html',
